f3856df0edf50e4e4652d573db9af6d0b5311f28
cristi <scarlat.cristian.gabriel@gmail.com>
2023-07-31 16:04:59 +0300

    added slide animation

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 "use strict";
 
-const btnNext = document.querySelectorAll(".nextStep");
+const btnNext = document.querySelectorAll(".next-step");
 const btnPrev = document.querySelectorAll(".prev-question");
+const helpPage = document.querySelector(".how-we-help");
+
+btnPrev.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    if (index === 0) {
+      helpPage.classList.remove("invisible-left");
+
+      document
+        .querySelector(`.page-${index + 1}`)
+        .classList.add("invisible-right");
+
+      return;
+    }
+
+    document.querySelector(`.page-${index}`).classList.remove("invisible-left");
+    document
+      .querySelector(`.page-${index + 1}`)
+      .classList.add("invisible-right");
+  });
+});
+
+btnNext.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    if (index !== 17) {
+      console.log(index);
+      document
+        .querySelector(`.page-${index + 3}`)
+        .classList.remove("invisible-right");
+
+      document
+        .querySelector(`.page-${index + 2}`)
+        .classList.add("invisible-left");
+    }
+  });
+});
